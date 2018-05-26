c5bc051921cde5f6e8564d21eee99e136521ccf4
Nghi <pthai.nghi@gmail.com>
2018-05-26 22:50:28 +0700

    Working Database Helper

--- app/src/main/java/apcs/gameuit/DairyActivity.java
+++ app/src/main/java/apcs/gameuit/DairyActivity.java
@@ -4,12 +4,15 @@ import android.app.ActionBar;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.GridLayout;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -34,21 +37,38 @@ public class DairyActivity extends AppCompatActivity {
     private void readStoriesFromDatabase() {
         MyDatabaseHelper dbHelper = new MyDatabaseHelper(this);
 
+        SQLiteDatabase dairy = dbHelper.getWritableDatabase();
+
 
         //dairy.execSQL("INSERT INTO dairy VALUES(1,'Fuck today', 'I fucking hate this stupid day')");
 
-        //Cursor cr = dairy.rawQuery("SELECT * FROM dairy",null);
 
-       // CREATE TABLE dairy(ID INTEGER PRIMARY KEY, TITLE TEXT,STORY TEXT ,CONTENT TEXT,DATE TEXT, IMAGE TEXT)
 
-        ContentValues values = new ContentValues();
-        values.put("TITLE","Fuck my life");
-        values.put("STORY","AAAAAAAAAAAAAAAA");
-        values.put("CONTENT","FUCKKKKKKKKKKK");
-        values.put("IMAGE","cute15");
+       // CREATE TABLE dairy( TITLE TEXT,STORY TEXT ,CONTENT TEXT,DATE TEXT, IMAGE TEXT)
+
+
 
         Date c = Calendar.getInstance().getTime();
         SimpleDateFormat df = new SimpleDateFormat("dd-mm-yyyy");
+        String formattedDate = df.format(c);
+
+        //String query = "INSERT INTO dairy VALUES('Fuck my life','AAAAAAA','Fuck this shit im out'," + formattedDate + ",'cute15')";
+
+
+        String query = "UPDATE dairy SET DATE='"+formattedDate+"'";
+        Log.d("date",query);
+        dairy.execSQL(query);
+
+//        long newRowId = dairy.insert("dairy",null,values);
+
+        //dairy.execSQL("DROP TABLE dairy");
+
+        Cursor cr = dairy.rawQuery("SELECT * FROM dairy",null);
+        cr.moveToNext();
+
+        displayThisDairy(cr);
+
+
         //String myDate
 
     }
@@ -57,8 +77,10 @@ public class DairyActivity extends AppCompatActivity {
 
         LinearLayout mainLayout = findViewById(R.id.dairymain);
 
-        String title = cr.getString(cr.getColumnIndex("TILTE"));
-        String content = cr.getString(cr.getColumnIndex("CONTENT"));
+        String title = cr.getString(cr.getColumnIndex("TITLE"));
+        String date = cr.getString(cr.getColumnIndex("DATE"));
+        String imageName = cr.getString(cr.getColumnIndex("IMAGE"));
+        int imageID = getResources().getIdentifier(imageName , "drawable", getPackageName());
 
 
         CardView dairyCard = new CardView(this);
@@ -67,6 +89,7 @@ public class DairyActivity extends AppCompatActivity {
         dairyCard.setLayoutParams(layoutParams);
 
         LinearLayout cardLinearLayout = new LinearLayout(this);
+        cardLinearLayout.setOrientation(LinearLayout.VERTICAL);
 
         cardLinearLayout.setOrientation(LinearLayout.VERTICAL);
         LinearLayout.LayoutParams linearLayoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
@@ -77,15 +100,25 @@ public class DairyActivity extends AppCompatActivity {
         dairyCard.setMaxCardElevation(30f);
 
         TextView titleBox = new TextView(this);
-        TextView contentBox = new TextView(this);
+        TextView dateBox = new TextView(this);
+        ImageView imageBox = new ImageView(this);
+
+        imageBox.setMaxHeight(100);
+        imageBox.setMaxWidth(100);
+        imageBox.setImageResource(imageID);
+
+        cardLinearLayout.addView(imageBox);
+
+
+
 
         titleBox.setText(title);
-        contentBox.setText(content);
+        dateBox.setText(date);
 
         titleBox.setTextSize(30f);
 
         cardLinearLayout.addView(titleBox);
-        cardLinearLayout.addView(contentBox);
+        cardLinearLayout.addView(dateBox);
 
         dairyCard.addView(cardLinearLayout);
 
--- app/src/main/java/apcs/gameuit/MyDatabaseHelper.java
+++ app/src/main/java/apcs/gameuit/MyDatabaseHelper.java
@@ -7,21 +7,18 @@ import android.database.sqlite.SQLiteOpenHelper;
 ;
 
 public class MyDatabaseHelper extends SQLiteOpenHelper{
-    private SQLiteDatabase database;
     public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "main";
+    public static final String DATABASE_NAME = "main.db";
 
     public MyDatabaseHelper(Context context){
         super(context, DATABASE_NAME,null,DATABASE_VERSION);
 
     }
 
-    @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE dairy(TITLE TEXT,STORY TEXT ,CONTENT TEXT,DATE TEXT, IMAGE TEXT)");
+        db.execSQL("CREATE TABLE dairy(TITLE TEXT,STORY TEXT,CONTENT TEXT,DATE TEXT,IMAGE TEXT)");
     }
 
-    @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS dairy");
         onCreate(db);
